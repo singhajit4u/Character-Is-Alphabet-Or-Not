895deff78790b00915a439199e744212126b86a8
NFO-2 <NFO-2@NFO-2>
2023-03-15 16:43:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Character_Is_Alphabet_Or_Not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the character to check it is Alphabet:");
+
+            char alphabet = Convert.ToChar(Console.ReadLine());
+            if ((alphabet >= 'a' && alphabet <= 'z') || (alphabet >= 'A' && alphabet <= 'Z'))
+            {
+
+                Console.WriteLine($"{alphabet}:is a alphabet");
+            }
+            else 
+            {
+                Console.WriteLine($"{alphabet}: is not a alphabet");
+            }
+        }
+    }
+}
